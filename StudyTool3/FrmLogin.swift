11ba444adde3c723a07db1b2133b0f94d17ff6e1
陈为 <chenwei2000121@163.com>
2015-09-18 14:48:48 +0800

    add third-parties

---
+++ StudyTool3/FrmLogin.swift
@@ -0,0 +1,22 @@
+//
+//  FrmLogin.swift
+//  StudyTool3
+//
+//  Created by 陈为 on 9/18/15.
+//  Copyright © 2015 David Chen. All rights reserved.
+//
+
+import UIKit
+
+class FrmLogin: UIViewController {
+
+    @IBOutlet weak var tbUsername: UITextField!
+    @IBOutlet weak var tbPassword: UITextField!
+    
+    @IBAction func btnLoginTapped(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
